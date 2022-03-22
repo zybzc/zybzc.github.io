9068a70064b3d41b0c7f590034fd1d58c2cc9873
zybcome <zybcome@sina.com>
2022-03-22 15:33:36 +0800

    提交代码

--- common/common.js
+++ common/common.js
@@ -1,6 +1,12 @@
 // 用于写一些公用方法，这个js全局都有引入
 window.onload = function(){
-
+  var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?a3be5ab26634acc19de079b4b1eaa520";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+  })();
 }
 
 /**
